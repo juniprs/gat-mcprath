3b7140bb1dd25c3d19189f3e9ca17f4dfd80821e
Jennifer Tran <cococlone@gmail.com>
2021-06-02 18:01:33 -0700

    Add session actions, store, and reducers

--- frontend/gat_mcprath.jsx
+++ frontend/gat_mcprath.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import * as SessionApiUtil from './util/session_api_util';
+import * as ApiUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     // let store;
@@ -19,10 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
     //     store = configureStore();
     // }
     let store = configureStore();
-    window.state = store.getState();
-    window.login = SessionApiUtil.login;
-    window.logout = SessionApiUtil.logout;
-    window.signup = SessionApiUtil.signup;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.login = ApiUtil.login;
+    window.logout = ApiUtil.logout;
+    window.signup = ApiUtil.signup;
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
 });
