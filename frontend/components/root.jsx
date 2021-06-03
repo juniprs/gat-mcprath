3d6cd70fbf89e61546a0a0e34beca826b4dc9b44
Jennifer Tran <cococlone@gmail.com>
2021-06-02 22:40:42 -0700

    Complete reducers and fix bugs

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
-// import { HashRouter } from 'react-router';
-
+import { HashRouter } from 'react-router';
 import App from "./app";
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
-      {/* <HashRouter> */}
+      <HashRouter>
       <App />
-      {/* </HashRouter> */}
+      </HashRouter>
     </Provider>
   );
 };
--- frontend/gat_mcprath.jsx
+++ frontend/gat_mcprath.jsx
@@ -5,20 +5,20 @@ import configureStore from './store/store';
 import * as ApiUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // let store;
-    // if (window.currentUser) {
-    //     const preloadedState = {
-    //         session: { id: window.currentUser.id },
-    //         entities: {
-    //             users: { [window.currentUser.id]: window.currentUser }
-    //         }
-    //     };
-    //     store = configureStore(preloadedState);
-    //     delete window.currentUser;
-    // } else {
-    //     store = configureStore();
-    // }
-    let store = configureStore();
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            session: { id: window.currentUser.id },
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+    window.store = store
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     window.login = ApiUtil.login;
