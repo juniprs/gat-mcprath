cb8113acee3fe8ca08523193943c9e22616fce02
Jennifer Tran <cococlone@gmail.com>
2021-06-04 21:08:10 -0700

    Remove debugger from file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
     def login(user)
         user.reset_session_token!
         session[:session_token] = user.session_token
-        # debugger
         @current_user = user
     end
 
