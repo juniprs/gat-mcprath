ec66f507c8b64b8c46c8606ac5c30befe0d023b0
Jennifer Tran <cococlone@gmail.com>
2021-06-30 18:25:32 -0700

    Add embedded video on splash

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import NavbarContainer from './navbar/navbar_container';
+import React from "react";
+import NavbarContainer from "./navbar/navbar_container";
 
 const Splash = () => (
-    <div>
-    </div>
+  <div>
+    <iframe
+      src="https://player.vimeo.com/video/515444240?autoplay=1&loop=1&title=0&byline=0&portrait=0&background=1"
+      width="640"
+      height="640"
+      frameborder="0"
+      allow="autoplay"
+    ></iframe>
+  </div>
 );
 
 export default Splash;
+
+<iframe
+  id="video-component"
+  data-src-large="https://player.vimeo.com/video/561790067?background=1"
+  data-src-small="https://player.vimeo.com/video/561790098?background=1"
+  allow="autoplay"
+  frameborder="0"
+  webkitallowfullscreen=""
+  mozallowfullscreen=""
+  allowfullscreen=""
+  class="src-small-active"
+  src="https://player.vimeo.com/video/561790098?background=1"
+></iframe>;<iframe
+  id="video-component"
+  data-src-large="https://player.vimeo.com/video/561790067?background=1"
+  data-src-small="https://player.vimeo.com/video/561790098?background=1"
+  allow="autoplay"
+  frameborder="0"
+  webkitallowfullscreen=""
+  mozallowfullscreen=""
+  allowfullscreen=""
+  class="src-small-active"
+  src="https://player.vimeo.com/video/561790098?background=1"
+></iframe>;
