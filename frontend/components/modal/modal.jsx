6f535e425e72eb1e42fbaba739fdafa133de704b
Jennifer Tran <cococlone@gmail.com>
2021-07-18 21:47:49 -0700

    Add basic styling to cart modal

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import { IoCloseOutline } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default (props) => {
-  if (!props.show) {
-    return null;
-  }
+  // if (!props.show) {
+  //   return null;
+  // }
 
   const closeOnEscape = (e) => {
     if ((e.charCode || e.keyCode) === 27) {
@@ -20,18 +20,20 @@ export default (props) => {
   }, []);
 
   return (
-    <div className="modal-wrapper" onClick={props.onClose}>
+    <div
+      className={`modal-wrapper ${props.show ? "show" : ""}`}
+      onClick={props.onClose}
+    >
       <div className="modal-container" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
-          {/* <IoCloseOutline /> */}
           <h1 className="modal-title">YOUR BAG</h1>
         </div>
-        <div className="modal-body">{props.children}</div>
-        <div className="modal-footer">
-          <button onClick={props.onClose} className="modal-close">
-            Close
-          </button>
-        </div>
+        {/* <div className="modal-body">{props.children}</div>
+         */}
+        <div className="modal-body">Your bag is currently empty.</div>
+        <a onClick={props.onClose} className="modal-close">
+          <IoCloseOutline size={20} />
+        </a>
       </div>
     </div>
   );
